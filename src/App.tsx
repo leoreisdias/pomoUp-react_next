4e363c7e80960077a9ffc2d4fb64bd14f362c674
leoreisdias <leonardord99@gmail.com>
2021-02-22 23:39:19 -0300

    Adicionado barra de experiencia estatica -> Lottie Files adicionado para animações

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import './App.css';
+import './styles/global.css';
+import ExperienceBar from './components/ExperienceBar';
+import Lottie from 'react-lottie';
+import animationData from './lotties/laptop-working.json';
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
-   <h1>Hello World</h1>
+    <div className="container">
+      <ExperienceBar />
+      <Lottie
+        options={defaultOptions}
+        height={'100%'}
+        width={'100%'}
+      />
+    </div>
   );
 }
 
