e7228e0a04d580152939f0403ab86bf5a30aedd5
leoreisdias <leonardord99@gmail.com>
2021-03-18 23:23:12 -0300

    fix: default timer in countdown restablished

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -25,7 +25,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
   const [isInPauseTime, setIsInPauseTime] = useState(false);
@@ -40,7 +40,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
     setIsInPauseTime(false);
   }
