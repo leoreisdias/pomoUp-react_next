fda033bcc9defbe13e02a31358e10927fccf4c10
leoreisdias <leonardord99@gmail.com>
2021-03-26 17:01:17 -0300

    style(component): ranking and usercard

--- src/components/UserBoardCard.tsx
+++ src/components/UserBoardCard.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useProfile } from '../contexts/ProfileContext';
 
 import styles from '../styles/components/UserBoardCard.module.css';
 import Profile from './Profile';
 
 const UserBoardCard = () => {
+  const { level } = useContext(ChallengesContext);
+  const { githubName, githubAvatar } = useProfile();
+
   return (
     <main className={styles.cardContainer}>
       <span>
         1
       </span>
-      <div>
-        <Profile />
-        <strong><span>24</span> completados</strong>
-        <strong><span>14000</span> xp</strong>
+      <div className={styles.profileContainer}>
+        <img src={String(githubAvatar)} alt=" Avatar" />
+        <div>
+          <strong>{githubName}</strong>
+          <p>
+            <img src="/icons/level.svg" alt="level" />
+                    Level {level}
+          </p>
+        </div>
       </div>
+      <strong><span>24</span> completados</strong>
+      <strong><span>14000</span> xp</strong>
     </main>
   );
 }
